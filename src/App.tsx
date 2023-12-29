1a8aadb8421cdf43d0a3b5a46367f0140ebda489
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2023-12-30 00:44:22 +0530

    Responsive Grid Leyout setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Button colorScheme="blue">Button</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"gold"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"darkgray"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
