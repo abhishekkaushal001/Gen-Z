6286b5267040cf606350bf102bab6bf93ac66d92
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2023-12-30 12:03:01 +0530

    Score Badge design.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import Game from "../interfaces/game";
 import PlatformIcons from "./PlatformIcons";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -12,10 +13,13 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack marginY={1}>
-          {game.parent_platforms.map(({ platform }) => (
-            <PlatformIcons key={platform.id} platforms={platform} />
-          ))}
+        <HStack marginY={1} justifyContent="space-between">
+          <HStack>
+            {game.parent_platforms.map(({ platform }) => (
+              <PlatformIcons key={platform.id} platforms={platform} />
+            ))}
+          </HStack>
+          <CriticScore score={game.metacritic} />
         </HStack>
       </CardBody>
     </Card>
