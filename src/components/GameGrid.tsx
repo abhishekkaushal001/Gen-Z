7e411e86d455d9bb72b50cc5753dd7118c679257
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-01-01 02:43:56 +0530

    Refactoring the Game grid.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,22 +16,21 @@ const GameGrid = ({ genre, platform, sort, serach }: Props) => {
   const { data, error, isLoading } = useGames(genre, platform, sort, serach);
   const skeletons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => <CardLoadingSkeleton key={skeleton} />)}
+  if (error) return <Text>{error}</Text>;
 
-        {data.map((g) => (
-          <GameCard game={g} key={g.id} />
-        ))}
-      </SimpleGrid>
-    </>
+  return (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => <CardLoadingSkeleton key={skeleton} />)}
+
+      {data.map((g) => (
+        <GameCard game={g} key={g.id} />
+      ))}
+    </SimpleGrid>
   );
 };
 
