e1e8413b9830cd0a7df9c7d89a3f9f3d9f937ee5
Abhishek Kaushal <abhishekkaushal121@gmail.com>
2024-01-02 11:38:54 +0530

    added Infinite Scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import CardLoadingSkeleton from "./skeletons/CardLoadingSkeleton";
 import Genres from "../interfaces/genres";
 import ParentPlatforms from "../interfaces/parentPlatforms";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   genre?: Genres;
@@ -14,14 +15,29 @@ interface Props {
 }
 
 const GameGrid = ({ genre, platform, sort, serach }: Props) => {
-  const { data, error, isLoading, isFetching, fetchNextPage, hasNextPage } =
-    useGames(genre, platform, sort, serach);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames(
+    genre,
+    platform,
+    sort,
+    serach
+  );
   const skeletons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   if (error) return <Text>{error.message}</Text>;
 
   return (
-    <Box padding="10px">
+    <InfiniteScroll
+      dataLength={(data?.pages.length || 0) * (data?.pages[0].count || 0)}
+      hasMore={hasNextPage}
+      loader={
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+          {skeletons.map((skeleton) => (
+            <CardLoadingSkeleton key={skeleton} />
+          ))}
+        </SimpleGrid>
+      }
+      next={() => fetchNextPage()}
+    >
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
         {isLoading &&
           skeletons.map((skeleton) => <CardLoadingSkeleton key={skeleton} />)}
@@ -34,13 +50,7 @@ const GameGrid = ({ genre, platform, sort, serach }: Props) => {
           </React.Fragment>
         ))}
       </SimpleGrid>
-
-      {hasNextPage && (
-        <Button onClick={() => fetchNextPage()} marginY={5}>
-          {isFetching ? "Loading.." : "load More"}
-        </Button>
-      )}
-    </Box>
+    </InfiniteScroll>
   );
 };
 
